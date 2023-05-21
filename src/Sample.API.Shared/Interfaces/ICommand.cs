7b43a60cd9ee1a98a660a638079eb49e4cbd3d25
Angelo Pirola <angelo9871@gmail.com>
2023-05-22 00:04:37 +0200

    Rimosso progetto Shared

--- src/Sample.API.Shared/Interfaces/ICommand.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Sample.API.Shared.Interfaces;
-
-public interface ICommand<TEntity> where TEntity : class
-{
-    Task CreateAsync(TEntity entity);
-    Task UpdateAsync(TEntity entity);
-    Task DeleteAsync(TEntity entity);
-}
--- src/Sample.API.Shared/Interfaces/IDatabase.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Sample.API.Shared.Interfaces;
-
-public interface IDatabase<T> where T : class
-{
-    Task<List<T>> GetAllAsync();
-    Task<T> GetByIdAsync(Guid id);
-}
